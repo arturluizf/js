bafe0b204b7a362724b34b060c27d1e79635fa40
Artur Luiz <arturfe23@gmail.com>
2024-11-01 11:20:25 -0300

    "Updated console.log statements in index.js"

--- udemy/aula1/index.js
+++ udemy/aula1/index.js
@@ -1,7 +1,7 @@
 console.log('Olá "mundo!"'); // string = texto
-console.log("Olá 'mundo!'"); //number = número
+console.log("Olá 'mundo!'"); 
 console.log(`'Olá' "mundo!"`); 
-
-console.log(10, 15.50,  'Olá mundo!'); 
-
+console.log(10) //number = número
+console.log('true ou false') //booelan
+console.log(10, 15.50,  'Olá mundo!');
 
