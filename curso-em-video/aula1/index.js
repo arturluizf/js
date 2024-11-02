2342819d4ade01b048edea1860691379d21b15f3
Artur Luiz <arturfe23@gmail.com>
2024-11-01 22:20:13 -0300

    muita coisa

---
+++ curso-em-video/aula1/index.js
@@ -0,0 +1,9 @@
+console.log('oi')//typeof = string
+console.log('10')//typeof = number
+console.log(true, false) //typeof = boolean
+//tem outros tipos de dados em js, como array, objeto, null e undefined
+//para saber um tipo de dados é só ir no terminal e usar typeof
+//podemos declarar uma variável com o let, const ou var
+
+
+
