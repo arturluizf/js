ce8a4782031add6dfcd09ad5910f4299d6520b8c
Artur Luiz <arturfe23@gmail.com>
2024-11-06 22:56:58 -0300

    condições em js

---
+++ curso-em-video/modulo-d/ex001.js
@@ -0,0 +1,8 @@
+var vel = 90
+console.log(`A velociadade do seu carro é ${vel} km/h.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. Você foi multado!.`)
+} else {
+    console.log(`Você está dentro do limite de velocidade.`)
+}
+console.log(`Dirija com segurança!`)
