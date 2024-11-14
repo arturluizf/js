a6733ddbb224d360320f1f20dc6751721bd9e2cc
Artur Luiz <arturfe23@gmail.com>
2024-11-13 21:51:47 -0300

    "Updated index.html title and script.js logic for counter functionality"

--- curso-em-video/modulo-e/praticando/ex01/script.js
+++ curso-em-video/modulo-e/praticando/ex01/script.js
@@ -4,17 +4,32 @@ let i = document.getElementById('inicio')
 let f = document.getElementById('fim') 
 let p = document.getElementById('passo')
 
-if (i.value.length == 0 || f.value == 0 || p.value == 0) {
-    alert('[Erro]')
-}else {
-    res.innerHTML = 'Contando:'
+if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
+    //alert('[Erro] Coloque um número válido')
+    res.innerHTML = 'Impossível contar!'
     res.style.marginTop = '10px'
-    let ini = Number(i.value)
-    let fim = Number(f.value)
-    let pas = Number(p.value)
+    } else {
+        res.innerHTML = 'Contando:'
+        res.style.marginTop = '10px'
+        let ini = Number(i.value)
+        let fim = Number(f.value)
+        let pas = Number(p.value)
+        if (pas <= 0 || ini <= 0 || fim <= 0) {
+            alert('Coloque um número diferente de 0')
+            pas = 1
+            ini = 1
+            fim = 1
+        }
+            if (ini < fim) /*Crescente*/ {
+                for (let c = ini; c <= fim; c += pas) {
+                    res.innerHTML += `${c} \u{27A1}`
+                }
+            } else  /*Decrescente*/ { 
+        for (let c = ini; c >= fim; c -= pas) {
+        res.innerHTML += `${c} \u{27A1}`
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
+    }
+}
 
-    for (let c = i; c <= fim; c += pas) {
-        res.innerHTML += `${c} `
-}
-}
-}
---
+++ curso-em-video/modulo-e/praticando/ex02/index.js
@@ -0,0 +1,28 @@
+function tabuada() {
+    let numero = document.getElementById('numero')
+    let tab = document.getElementById('tabuada')
+    if (numero.value.length == 0) {
+        alert('Por favor, preencha o campo com um número')
+    } else { 
+        let num = Number(numero.value)
+        tab.innerHTML = ''
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${i} = ${num * i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+    }
+}
+}
+
+
+/*{
+        let num = Number(numero.value)
+        tab.innerHTML = ''
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${i} = ${num * i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    } */
